eda0003a77b0432e9e70deefeacda9374958ff00
Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>
2021-06-30 00:40:20 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # aiosre-docker
-All In One SRE Docker Container
+## All In One SRE Docker Container
+
+For all my SRE needs, neatly packaged in a Docker container.
+
+## default:
+
+`docker run -it mathisve/aiosre`
+
+<!-- ## with AWS Credentials
+
+`docker run -it -V ~/.aws:/home/root/.aws mathisve/aiosre` -->
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update
+apt-get install sudo net-tools apt-utils htop wget curl unzip build-essential -y
+
+# AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+
+# Terraform 
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt install terraform -y
+
+# Kubectl
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get install -y kubectl
+
+apt-get upgrade -y
