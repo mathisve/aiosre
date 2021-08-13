7d951901a2925cbb721185425b7ed84446170571
Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>
2021-08-13 15:22:23 +0200

    Pull development branch into master (#2)
    * Merge move-away-from-apt with development branch (#1)
    
    * added eksctl
    
    * updated install.sh
    
    * updated ownership
    
    * added new workflows
    
    * removed login in build workflow
    
    * update name of step in build workflow
    
    * add on pr to build workflow

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install sudo net-tools apt-utils htop wget curl unzip build-essential -y
+apt-get install sudo apt-utils net-tools git htop wget curl unzip build-essential make -y
+apt-get upgrade -y
+apt-get autoremove
 
 # AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -9,15 +11,30 @@ unzip awscliv2.zip
 sudo ./aws/install
 rm awscliv2.zip
 
+aws --version
+
+
 # Terraform 
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt install terraform -y
+wget https://releases.hashicorp.com/terraform/1.0.4/terraform_1.0.4_linux_amd64.zip
+unzip terraform_*.zip
+sudo mv terraform /usr/local/bin
+rm -f terraform*
+
+terraform --version
+
 
 # Kubectl
-sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get install -y kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-apt-get upgrade -y
+kubectl version --client
+
+
+# eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+# rm eksctl_*_amd64.tar.gz
+
+eksctl version
