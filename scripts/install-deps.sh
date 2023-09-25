e7df9957cee67b3af330ba6489f86fb617e7391b
Mathis Van Eetvelde <mathis.vaneetvelde@protonmail.com>
2023-09-25 12:16:29 -0600

    Add jq

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,6 +2,6 @@
 
 echo "Installing apt packages"
 apt-get -qq update
-apt-get -qq install sudo apt-utils net-tools git htop nano wget dnsutils curl unzip -y
+apt-get -qq install sudo apt-utils net-tools git htop nano wget dnsutils curl unzip jq -y
 apt-get -qq upgrade -y
 apt-get -qq autoremove
